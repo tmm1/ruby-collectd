f17f347d896681f7c4f69d3990338aed3a2bedb1
Astro <astro@spaceboyz.net>
2009-05-29 02:47:00 +0200

    sender: Thread.pass to switch to collectd thread

--- examples/sender.rb
+++ examples/sender.rb
@@ -7,6 +7,7 @@ loop do
   Collectd.ruby_collectd(:test).ping(:fun).gauge = 42 + Math.sin(Time.now.to_f / 600) * 23.5
   Collectd.ruby_collectd(:test).cpu(:time).counter = c
   c += 1
+  Thread.pass
   sleep 0.01
   print '.'
   STDOUT.flush
