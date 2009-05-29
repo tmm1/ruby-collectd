a52ff1fbdb35a8a552d99dec3d26f494588afc7b
Astro <astro@spaceboyz.net>
2009-05-29 02:48:01 +0200

    examples: use documented conventions

--- examples/em_sender.rb
+++ examples/em_sender.rb
@@ -4,11 +4,13 @@ require 'collectd'
 EM.run do
   Collectd::use_eventmachine = true
   Collectd::add_server 1
+  Stats = Collectd.ruby_collectd(:test)
+  Stats.with_full_proc_stats
 
   c = 0
   EM.add_periodic_timer(0.01) do
-    Collectd.ruby_collectd(:test).ping(:fun).gauge = 42 + Math.sin(Time.now.to_f / 600) * 23.5
-    Collectd.ruby_collectd(:test).cpu(:time).counter = c
+    Stats.ping(:fun).gauge = 42 + Math.sin(Time.now.to_f / 600) * 23.5
+    Stats.cpu(:time).counter = c
 
     c += 1
     print '.'
--- examples/sender.rb
+++ examples/sender.rb
@@ -1,11 +1,13 @@
 require 'collectd'
 
 Collectd::add_server 1
+Stats = Collectd::ruby_collectd(:test)
+Stats.with_full_proc_stats
 
 c = 0
 loop do
-  Collectd.ruby_collectd(:test).ping(:fun).gauge = 42 + Math.sin(Time.now.to_f / 600) * 23.5
-  Collectd.ruby_collectd(:test).cpu(:time).counter = c
+  Stats.ping(:fun).gauge = 42 + Math.sin(Time.now.to_f / 600) * 23.5
+  Stats.cpu(:time).counter = c
   c += 1
   Thread.pass
   sleep 0.01
